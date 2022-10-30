5ec5a0bae11a8e23db183c036c7b6e372824a664
Daniyar Hapurzhonau <57048851+hapurzhonau@users.noreply.github.com>
2022-10-30 17:39:35 +0300

    Add files via upload

---
+++ absolute value.js
@@ -0,0 +1,10 @@
+const absolute = (a) => {
+  //finding absolute value of the number
+  if (a >= 0) {
+    return console.log(`(${a}) -- Number is not negative.`);
+  } else a * -1 >= 0;
+  {
+    return console.log(`(${a}) --> (${a * -1}) -- Number became positive.`);
+  }
+};
+absolute(-9); //enter any number in the breckets
